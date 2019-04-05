6c22127b526464cd0c1ee3ea23e3762ec849df62
Afonso Barbosa <afonso33barbosa@gmail.com>
2019-04-05 19:10:57 -0300

    pagina do instituto criada

--- send-email.php
+++ send-email.php
@@ -1,46 +1,31 @@
 <?php 
-
 if(isset($_POST['nome']) && !empty($_POST['nome'])
 	&& isset($_POST['email']) && !empty($_POST['email'])
 	&& isset($_POST['telefone']) && !empty($_POST['telefone'])
 	&& isset($_POST['assunto']) && !empty($_POST['assunto'])
 	&& isset($_POST['menssagem']) && !empty($_POST['menssagem'])){
-
 	$nome = addslashes($_POST['nome']);
 	$telefone = addslashes($_POST['telefone']);
 	$email = addslashes($_POST['email']);
 	$assunto = addslashes($_POST['assunto']);
 	$menssagem = addslashes($_POST['menssagem']);
-
 	$to = 'afonso33barbosa@gmail.com';
 	$assunto_postal = 'Portfólio -> '.$assunto;
 	$body = "Nome: ".$nome. "\r\n"
 								 ."Email: ".$email. "\r\n"
 								 ."Telefone: ".$telefone. "\r\n"
 								 ."menssagem: ".$menssagem; 
-
 	// $header = 'From:port.butterfly@theevolution.com'."\r\n"
 	// 					."Replay-To:".$email."\r\n"
 	// 					."X=Mailer:PHP/".phpversion();
-
-
 	if(mail($to, $assunto_postal, $body)){
-		echo "<script>
-						location.href='thanks.html';
-					</script>";
+		echo "<Script>location.href='thanks.html';</script>";
 	}
 	else{
-		echo "<script>
-						alert('Não foi possível enviar o seu email. Tente novamente mais tarde.');
-						location.href='index.html#contato';
-					</script>"
+		echo "<script>alert('Não foi possível enviar seu email. Tente novamente mais tarde.');</script>";
 	}
 }
 else{
-	echo "<script>
-					alert('preencha os campos corretamente.');
-					location.href='index.html#contact';
-				</script>";
+	echo "<script>alert('preencha os campos corretamente'); location.href='index.html#contact';</script>";
 }
-
  ?>
