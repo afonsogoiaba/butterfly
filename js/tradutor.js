75b8ad8c66cfac48d6270fa08dee44d66d2b81b3
Afonso Barbosa <afonso33barbosa@gmail.com>
2019-04-06 00:27:38 -0300

    adicionando traduções

--- js/tradutor.js
+++ js/tradutor.js
@@ -138,6 +138,61 @@ $(function() {
       pt: "Em Progresso"
     },
 
+    "This project was developed during the first semester of 2017 to obtain a note on the subject of Presentation Language and Content Structuring 1. It was the final work of the subject with the objective of developing a portfolio of Web Designer.":{
+      en: "This project was developed during the first semester of 2017 to obtain a note on the subject of Presentation Language and Content Structuring 1. It was the final work of the subject with the objective of developing a portfolio of Web Designer.",
+      pt: "Esse projeto foi desenvolvido durante o primeiro semestre de 2017 para obtenção de nota na disciplina de Linguagem de Apresentação e Estruturação de Conteúdos 1. Foi o trabalho final da matéria com o objetivo de desenvolver uma portfólio de Web Designer."
+    },
+
+    "This project was developed during the first semester of 2017 to obtain a grade in the subject of Presentation Language and Content Structuring 1. Its objective was to make students produce a portfolio based on the knowledge learned in the classroom.":{
+      en: "This project was developed during the first semester of 2017 to obtain a grade in the subject of Presentation Language and Content Structuring 1. Its objective was to make students produce a portfolio based on the knowledge learned in the classroom.",
+      pt: "Este projeto foi desenvolvido durante o primeiro semestre de 2017 para obter uma nota na matéria de Linguagem de Apresentação e Estruturação de Conteúdo 1. Seu objetivo era fazer com que os alunos produzissem um portfólio baseado no conhecimento aprendido em sala de aula."
+    },
+
+    "Limbo Driver was a system proposal for the second half of 2017 to get a grade in the Discipline of Presentation and Content Structuring 2 language. It works like Google Drive, but with a token access system, discarding user and password for tasks simple.":{
+      en: "Limbo Driver was a system proposal for the second half of 2017 to get a grade in the Discipline of Presentation and Content Structuring 2 language. It works like Google Drive, but with a token access system, discarding user and password for tasks simple.",
+      pt: "O Limbo Driver foi uma proposta de sistema para o segundo semestre de 2017 para obter uma nota na disciplina de Disciplina de Apresentação e Estruturação de Conteúdos 2. Funciona como o Google Drive, mas com um sistema de acesso por token, descartando usuário e senha para tarefas simples."
+    },
+
+    "Yago e Aldair are a cover duo known in the Dourados / MS region. They did a show in the space Quintal Espetos and to publicize the place, they needed an art for a banner that would spread by the city. This was the first job I did as Graphic Designer.":{
+      en: "Yago e Aldair are a cover duo known in the Dourados / MS region. They did a show in the space Quintal Espetos and to publicize the place, they needed an art for a banner that would spread by the city. This was the first job I did as Graphic Designer.",
+      pt: "Yago e Aldair são uma dupla de covers conhecida na região de Dourados / MS. Eles fizeram um show no espaço Quintal Espetos e para divulgar o local, precisavam de uma arte para um banner que se espalharia pela cidade. Este foi o primeiro trabalho que fiz como Designer Gráfico."
+    },
+
+    "Portfolio of the stars was a project developed during the second semester of 2017 as final work of the discipline of Language of Presentation and Structuring of Contents 2. The goal was to create a responsive web page applying the knowledge gained in both subjects.":{
+      en: "Portfolio of the stars was a project developed during the second semester of 2017 as final work of the discipline of Language of Presentation and Structuring of Contents 2. The goal was to create a responsive web page applying the knowledge gained in both subjects.",
+      pt: "Portfólio das estrelas foi um projeto desenvolvido durante o segundo semestre de 2017 como trabalho final da disciplina de Linguagem de Apresentação e Estruturação de Conteúdos 2. O objetivo era criar uma página web responsiva, aplicando o conhecimento adquirido nas duas matérias."
+    },
+
+    "Portwiki was my course completion work from highschool (TCC). His idea was based on the largest programming learning platform: w3schools.com, but intended to function as a mini-course management platform for the federal institute in question.": {
+      en: "Portwiki was my course completion work from highschool (TCC). His idea was based on the largest programming learning platform: w3schools.com, but intended to function as a mini-course management platform for the federal institute in question.",
+      pt: "Portwiki foi o meu trabalho de conclusão de curso do ensino médio (TCC). Sua idéia foi baseada na maior plataforma de aprendizado de programação: w3schools.com, mas destinada a funcionar como uma plataforma de gerenciamento de mini-cursos para o instituto federal em questão."
+    },
+
+    "The Sodelivery Express platform project was obtained as Freelancer. The client wanted a Brazilian platform at the national level of delivery of goods with motorcycles, trucks and freighters. However, as it was the first job as a freelancer and was very extensive, laborious and done by a team of 4 people, it was not possible to complete it.":{
+      en: "The Sodelivery Express platform project was obtained as Freelancer. The client wanted a Brazilian platform at the national level of delivery of goods with motorcycles, trucks and freighters. However, as it was the first job as a freelancer and was very extensive, laborious and done by a team of 4 people, it was not possible to complete it.",
+      pt: "O projeto da plataforma Sodelivery Express foi obtido como Freelancer. O cliente queria uma plataforma brasileira no nível nacional de entrega de mercadorias com motocicletas, caminhões e cargueiros. No entanto, como foi o primeiro trabalho como freelancer e foi muito extenso, trabalhoso e feito por uma equipe de 4 pessoas, não foi possível concluí-lo."
+    },
+
+    "The Sodelivery Express platform project was obtained as Freelancer. The client wanted a Brazilian platform at the national level of delivery of goods with motorcycles, trucks and freighters. However, as it was the first job as a freelancer and was very extensive ...":{
+      en: "The Sodelivery Express platform project was obtained as Freelancer. The client wanted a Brazilian platform at the national level of delivery of goods with motorcycles, trucks and freighters. However, as it was the first job as a freelancer and was very extensive ...",
+      pt: "O projeto da plataforma Sodelivery Express foi obtido como Freelancer. O cliente queria uma plataforma brasileira no nível nacional de entrega de mercadorias com motocicletas, caminhões e cargueiros. No entanto, como foi o primeiro trabalho como freelancer e foi muito extenso ..."
+    },
+
+    "This portfolio was started to be developed on February 12, 2019 and was finalized on April 6 , 2019. Its purpose beyond serving as a repository of projects and a way to demonstrate my work to my clients in a few clicks is also a timeline of evolutionary scale as a programmer. It is possible to see projects from when I started in the area, to the most current ones already completed or in development.":{
+      en: "This portfolio was started to be developed on February 12, 2019 and was finalized on April 6 , 2019. Its purpose beyond serving as a repository of projects and a way to demonstrate my work to my clients in a few clicks is also a timeline of evolutionary scale as a programmer. It is possible to see projects from when I started in the area, to the most current ones already completed or in development.",
+      pt: "Este portfólio, foi começou a ser desenvolvido em 12 de fevereiro de 2019 e foi finalizado em 6 de abril de 2019. Seu objetivo além de servir como repositório de projetos e uma forma de demonstrar meu trabalho para meus clientes em poucos cliques, é também uma linha do tempo em escala de evolução como programador. Nele é possível ver os projetos desde quando comecei na área, a até os mais atuais ja finalizados ou em desenvolvimento."
+    },
+
+    "This portfolio was started to be developed on February 12, 2019 and was finalized on April 6 , 2019. Its purpose beyond serving as a repository of projects and a way to demonstrate my work to my clients in a few clicks is also a timeline of evolutionary scale as a programmer ...":{
+      en: "This portfolio was started to be developed on February 12, 2019 and was finalized on April 6 , 2019. Its purpose beyond serving as a repository of projects and a way to demonstrate my work to my clients in a few clicks is also a timeline of evolutionary scale as a programmer ...",
+      pt: "Este portfólio, foi começou a ser desenvolvido em 12 de fevereiro de 2019 e foi finalizado em 6 de abril de 2019. Seu objetivo além de servir como repositório de projetos e uma forma de demonstrar meu trabalho para meus clientes em poucos cliques, é também uma linha do tempo em escala de evolução como programador ..." 
+    },
+
+    "Hi, I'm 18 years old and I'm in the last semester of the technical course integrated in internet computing. I am currently working as a freelancer and looking for a job. I have ease in creating layouts because much of my knowledge is front-end. However, I study enough Back-End to become an increasingly complete professional and get more complex jobs.": {
+      en: "Hi, I'm 18 years old and I'm in the last semester of the technical course integrated in internet computing. I am currently working as a freelancer and looking for a job. I have ease in creating layouts because much of my knowledge is front-end. However, I study enough Back-End to become an increasingly complete professional and get more complex jobs.",
+      pt: "Olá, tenho 18 anos e estou no último semestre do curso técnico integrado em informática para internet do IFMS - Dourados. Atualmente desenvolvo trabalhos como Freelancer e estou a procura de um emprego. Tenho facilidade em desenvolver layouts pois grande parte do meu conhecimento é Front-End. No entanto, estudo bastante Back-End para me tornar um profissional cada vez mais completo e pegar trabalhos mais complexos."
+    },
+
     "Development Front-end": {
       en: "Development Front-end",
       pt: "Desenvolvimento Front-end"
@@ -181,6 +236,11 @@ $(function() {
     "The evolution comes from the soul, a place in itself where few can explore. Face the stagnancy, the will to remain in the comfort zone and react. The first step to evolve is to seek the best part of yourself and improves to the maximum, several times. Know that the path to growth requires sacrifice that goes far beyond the physical and mental effort. Transcend.":{
       en: "The evolution comes from the soul, a place in itself where few can explore. Face the stagnancy, the will to remain in the comfort zone and react. The first step to evolve is to seek the best part of yourself and improves to the maximum, several times. Know that the path to growth requires sacrifice that goes far beyond the physical and mental effort. Transcend.",
       pt: "A evolução vem da alma, do lugar onde poucos conseguem explorar em si mesmo. Enfrente a estagnação, a vontade de permanecer na zona de conforto e reaja. O primeiro passo para evoluir é buscar a melhor parte de si e melhora-la ao máximo, diversas e diversas vezes. Enxergue que o caminho para o crescimento requer sacrifícios que vão muito além do esforço físico e mental. Transcenda."
+    },
+
+    "Development by <a href='https://github.com/afonsogoiaba' target='_blank'>Afonso Barbosa.</a>":{
+      en: "Development by <a href='https://github.com/afonsogoiaba' target='_blank'>Afonso Barbosa.</a>",
+      pt: "Desenvolvido por <a href='https://github.com/afonsogoiaba' target='_blank'>Afonso Barbosa.</a>"
     }
   };
   var _t = $('body').translate({lang: "en", t: t});
